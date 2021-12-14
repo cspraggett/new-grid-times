805538ea8274434312693f0373686f8d14c61c98
cspraggett <cspraggett@gmail.com>
2021-12-14 10:31:06 -0500

    Push avatar in opinion section to right side for mobile and desktop

--- src/components/OpinionStory/OpinionStory.js
+++ src/components/OpinionStory/OpinionStory.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import styled from 'styled-components/macro';
+import {QUERIES} from '../../constants';
 
 const OpinionStory = ({ id, title, author, avatar }) => {
   return (
@@ -17,17 +18,31 @@ const OpinionStory = ({ id, title, author, avatar }) => {
 
 const Wrapper = styled.article`
   color: var(--color-gray-900);
+  display: grid;
+  grid-template-areas:
+  "name avatar"
+  "title avatar";
+  grid-template-columns: 1fr auto;
+  gap: 16px;
+
+  @media ${QUERIES.tabletOnly} {
+    display: revert;
+  }
 `;
 
 const Avatar = styled.img`
   display: block;
+  grid-area: avatar;
+  align-self: center;
   width: 48px;
   height: 48px;
   border-radius: 50%;
   object-fit: cover;
+  margin-left: 32px;
 `;
 
 const AuthorName = styled.p`
+  grid-area: name;
   font-size: 1.125rem;
   font-weight: var(--font-weight-medium);
   color: var(--color-gray-700);
@@ -35,6 +50,7 @@ const AuthorName = styled.p`
 `;
 
 const ArticleTitle = styled.h3`
+  grid-area: title;
   font-size: 1.125rem;
   font-weight: var(--font-weight-bold);
   line-height: 1.3;
